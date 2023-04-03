72d459e678fd8fa2de9aec8552b2d5d024d547cf
nxyt <lolnoxy@gmail.com>
2023-04-03 11:45:05 +0200

    init

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module nix-docker-example-backend
+
+go 1.20
+
+require github.com/gorilla/mux v1.8.0
---
+++ backend/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	address := "0.0.0.0"
+	port := "7001"
+	router := mux.NewRouter()
+
+	router.HandleFunc("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
+
+		vars := mux.Vars(r)
+		name := vars["name"]
+		fmt.Fprintln(w, "Hello ", name)
+	})
+	log.Println("Starting server")
+	if err := http.ListenAndServe(address+":"+port, router); err != nil {
+		log.Panic((err))
+	}
+}
